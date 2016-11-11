1f26a21288541973928f6ad16e633181186f8c70
LeGitzR <linde.henry@gmail.com>
2016-11-11 10:59:02 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-function prevSlide() {
-    console.log("GeTS HERE");
-    $('.carousel').carousel();
-}
-
-function nextSlide() {
-    $('.carousel').carousel('next');
-}
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 4000); // Change image every 6 seconds
+};
